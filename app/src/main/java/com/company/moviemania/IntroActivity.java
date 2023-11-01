61942ca86cd6476da3b96c0c6b043fc32d9c76eb
Ansal Pandey <pandeyansal77@gmail.com>
2023-11-01 06:46:51 +0530

    Application UI changes

---
+++ app/src/main/java/com/company/moviemania/IntroActivity.java
@@ -0,0 +1,28 @@
+package com.company.moviemania;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class IntroActivity extends AppCompatActivity {
+    Button getStarted;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        getStarted = findViewById(R.id.get_started);
+
+        getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(IntroActivity.this, SignInActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/company/moviemania/SignInActivity.java
@@ -0,0 +1,44 @@
+package com.company.moviemania;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SignInActivity extends AppCompatActivity {
+    Button signIn;
+    EditText editTextUsername, editTextPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+
+        initView();
+    }
+
+    private void initView() {
+        editTextUsername = findViewById(R.id.editTextUserName);
+        editTextPassword = findViewById(R.id.editTextUserPassword);
+        signIn = findViewById(R.id.sign_in);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editTextUsername.getText().toString().isEmpty() || editTextPassword.getText().toString().isEmpty()) {
+                    Toast.makeText(SignInActivity.this, "Please enter data", Toast.LENGTH_SHORT).show();
+                } else if (editTextUsername.getText().toString().equals("admin") && editTextPassword.getText().toString().equals("admin")) {
+                    startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                    finish();
+                }
+                else {
+                    Toast.makeText(SignInActivity.this, "Your username or password is incorrect", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
